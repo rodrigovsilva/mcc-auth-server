43811fe859dcc9b4f4d2a84326a285f70325ce6d
BR\ztim <ztim@br.com.br [http]>
2018-06-15 16:04:24 -0300

    Updating dependencies and tests classes

--- src/test/java/com/rvfs/challenge/mcc/MccAuthServerApplicationTests.java
+++ src/test/java/com/rvfs/challenge/mcc/MccAuthServerApplicationTests.java
@@ -1,5 +1,6 @@
 package com.rvfs.challenge.mcc;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -10,6 +11,10 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @SpringBootTest(classes = MccAuthServerApplication.class)
 public class MccAuthServerApplicationTests {
 
+    @Test
+    public void contextLoads() {
+
+    }
     /*
     @Autowired
     private WebApplicationContext wac;
